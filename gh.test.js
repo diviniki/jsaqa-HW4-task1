71af3e6c041224ce48dce965463b0a5df47fc2e8
dmitriyn <d.nikitin.rf@gmail.com>
2025-01-14 17:53:43 +0500

    init commit

---
+++ gh.test.js
@@ -0,0 +1,73 @@
+let page;
+
+
+describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    //const browser = await puppeteer.launch({headless:false,SlowMo:20000});
+    page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(60000);
+    await page.goto("https://github.com/team", {waitUntil: 'load', timeout: 60000});
+  });
+  
+  afterEach(() => {
+    page.close();
+  });
+
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    //expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
+    //expect(title2).toEqual('GitHub for teams · Build like the best teams on the planet · GitHub');
+    expect(title2).toEqual('GitHub · Build and ship software on a single, collaborative platform · GitHub');
+  });
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  });
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    //expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Get started with Team")
+  });
+});
+
+
+describe("Additional tests", () => {
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(60000);
+    await page.goto("https://github.com/features/copilot", {waitUntil: 'load', timeout: 30000});
+  });
+  
+  afterEach(() => {
+    page.close();
+  });
+
+  test("Additional test 1", async () => {
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub Copilot · Your AI pair programmer · GitHub');
+  });
+  
+  test("Additional test 2", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub · Build and ship software on a single, collaborative platform · GitHub');
+  });
+
+  test("Additional test 3", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  });
+});
